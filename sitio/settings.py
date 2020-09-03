cfb10bac142eb8a55ca48770b0d08bffa59f84a1
JorgePinargote <jorge.apif@gmail.com>
2020-09-03 02:54:46 -0500

    Update settings.py

--- sitio/settings.py
+++ sitio/settings.py
@@ -1,4 +1,11 @@
 import dj_database_url
+
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
+
 DATABASES['default'] = dj_database_url.config()
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
@@ -6,8 +13,3 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
 DEBUG = False
-
-try:
-    from .local_settings import *
-except ImportError:
-    pass
